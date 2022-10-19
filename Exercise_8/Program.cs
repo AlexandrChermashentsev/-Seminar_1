6d0a4809528f11634fa2668eb2fc6ed991ed33ea
Alexandr.Cherm <Alexandr.chermashentsev@gmail.com>
2022-10-19 14:03:15 +0500

    Add Exercise_8

---
+++ Exercise_8/Program.cs
@@ -0,0 +1,34 @@
+﻿/*Задача 8: Напишите программу, которая на вход принимает число (N),
+а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+Console.WriteLine("Enter the number");
+string numberSTR = Console.ReadLine();
+int number = int.Parse(numberSTR);
+int index = 0;
+
+if (number == 0) Console.WriteLine("Вы ввели 0, введите другое число");
+if (number > 0)
+{
+    index = 1;
+    while (index <= number)
+    {
+        if (index % 2 == 0) Console.Write($"{index}, ");
+        index++;
+    }
+}
+else 
+{
+    if (number < 0)
+    {
+        index = -1;
+        while (index >= number)
+        {
+            if (index % 2 == 0) Console.Write($"{index}, ");
+            index--;
+        }
+    }
+}
